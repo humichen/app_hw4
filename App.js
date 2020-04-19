6a02c8ef88ac9be9304ed8029546b084b921f157
humichen <43415952+humichen@users.noreply.github.com>
2020-04-19 21:06:53 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { StyleSheet, TouchableOpacity, Image, View, Text } from "react-native";
-import { NavigationContainer,DrawerActions } from '@react-navigation/native';
+import { NavigationContainer, DrawerActions } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
 
 import AlbumScreen from './src/screens/AlbumScreen';
 
-function CustomDrawerContent(props) {
+const CustomDrawerContent = ({ ...props }) => {
   return (
     <DrawerContentScrollView {...props}>
       <View style={styles.headerStyle}>
@@ -28,79 +29,156 @@ function CustomDrawerContent(props) {
           />
         </View>
       </View>
-      <View style={styles.menubox}>
-          <Image
-            style={styles.menuimg}
-            source={{
-              uri: "https://github.com/humichen/app_hw4/blob/master/src/img/icon_drawer_home.png?raw=true"
-            }}
-          />
-          <Text style={styles.menutext}>Home</Text>
-        </View>
-      <DrawerItemList {...props} style={{width:304,height:54,backgroundColor:"#00b49f"}}/>
+     
+      <DrawerItemList {...props} />
 
-        <View style={styles.menubox}>
-          <Image
-            style={styles.menuimg}
-            source={{
-              uri: "https://github.com/humichen/app_hw4/blob/master/src/img/icon_drawer_favorites.png?raw=true"
-            }}
-          />
-          <Text style={styles.menutext}>Favorites</Text>
-        </View>
-        <View style={styles.menubox}>
-          <Image
-            style={styles.menuimg}
-            source={{
-              uri: "https://github.com/humichen/app_hw4/blob/master/src/img/icon_drawer_setting.png?raw=true"
-            }}
-          />
-          <Text style={styles.menutext}>Settings</Text>
-        </View>
-        <View style={styles.menubox}>
-          <Image
-            style={styles.menuimg}
-            source={{
-              uri: "https://github.com/humichen/app_hw4/blob/master/src/img/icon_drawer_aboutus.png?raw=true"
-            }}
-          />
-          <Text style={styles.menutext}>About us</Text>
-        </View>
     </DrawerContentScrollView>
   )
 }
 const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
-const App =()=>{
-  return (
-    <NavigationContainer>
+const App = () => {
+  return (   
+      <NavigationContainer>
       <Drawer.Navigator
-        initialRouteName="Home"
+        initialRouteName="My Book"
         drawerStyle={{
           width: 304,
           // height: 640,
           backgroundColor: "#ebebeb"
         }}
         drawerContent={props => <CustomDrawerContent {...props}
-        laybelStyle={{ color: "#00b49f", fontWeight: "600"}} />}
-        drawerContentOptions={{activeBackgroundColor:"#00b49f",activeTintColor:"#fff",inactiveBackgroundColor:"#ebebeb",inactiveTintColor:"#5c5c5c"}}
+          laybelStyle={{ color: "#00b49f", fontWeight: "600" }} />}
+        drawerContentOptions={{ activeBackgroundColor: "#00b49f", activeTintColor: "#fff",  itemStyle: { marginLeft: 0, marginRight: 0, borderRadius: 0 } }}
       >
+        <Drawer.Screen
+          name="Home"
+          component={AlbumScreen}
+          options={{
+            drawerIcon: ({focused }) => {
+              let iconPath;
+              iconPath=focused?require('./src/img/icon_drawer_home_pressed.png'):require('./src/img/icon_drawer_home.png');
+              return (
+              <Image
+                style={styles.menuimg}
+                source={iconPath}
+              />
+              );
+             }
+          }}
+        />
         <Drawer.Screen
           name="My Book"
           component={AlbumScreen}
           options={{
-            drawerIcon:({})=>(
+            drawerIcon: ({focused }) => {
+              let iconPath;
+              iconPath=focused?require('./src/img/icon_drawer_mybook_pressed.png'):require('./src/img/icon_drawer_mybook.png');
+              return (
               <Image
-              style={styles.menuimg2}
-              source={{
-                uri: "https://github.com/humichen/app_hw4/blob/master/src/img/icon_drawer_mybook_pressed.png?raw=true"
-              }}
-            />
-            )
+                style={styles.menuimg}
+                source={iconPath}
+              />
+              );
+             }
           }}
         />
+        <Drawer.Screen
+          name="Favorites"
+          component={AlbumScreen}
+          options={{
+            drawerIcon: ({focused }) => {
+              let iconPath;
+              iconPath=focused?require('./src/img/icon_drawer_favorites_pressed.png'):require('./src/img/icon_drawer_favorites.png');
+              return (
+              <Image
+                style={styles.menuimg}
+                source={iconPath}
+              />
+              );
+             }
+          }}
+        />
+        <Drawer.Screen
+          name="Settings"
+          component={AlbumScreen}
+          options={{
+            drawerIcon: ({focused }) => {
+              let iconPath;
+              iconPath=focused?require('./src/img/icon_drawer_setting_pressed.png'):require('./src/img/icon_drawer_setting.png');
+              return (
+              <Image
+                style={styles.menuimg}
+                source={iconPath}
+              />
+              );
+             }
+          }}
+        />
+        <Drawer.Screen
+          name="About us"
+          component={AlbumScreen}
+          options={{
+            drawerIcon: ({focused }) => {
+              let iconPath;
+              iconPath=focused?require('./src/img/icon_drawer_aboutus_pressed.png'):require('./src/img/icon_drawer_aboutus.png');
+              return (
+              <Image
+                style={styles.menuimg}
+                source={iconPath}
+              />
+              );
+             }
+          }}
+        />
+
       </Drawer.Navigator>
     </NavigationContainer>
+    /* <NavigationContainer>
+    <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconPath;
+
+            if (route.name === 'Album') {
+              iconPath = focused
+              ? require('./src/img/icon_bottomnav_home.png'):
+              require('./src/img/icon_bottomnav_home.png');
+            } else if (route.name === 'Settings') {
+              iconPath = focused
+              ? require('./src/img/icon_bottomnav_mybook_selected.png'):
+              require('./src/img/icon_drawer_mybook.png');
+            } else if (route.name == 'Me') {
+              iconPath = focused
+              ? require('./src/img/icon_bottomnav_favorites.png'):
+              require('./src/img/icon_bottomnav_favorites.png');
+            }
+
+            // You can return any component that you like here!
+            return (
+              <Image 
+                style={{width: 24, height: 24}}
+                source={iconPath} 
+              />
+            );
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#01B49F',
+          inactiveTintColor: 'gray',
+          labelStyle: {
+            fontSize: 12,
+            marginTop: 0,
+            padding: 0,
+          },
+        }}
+      >
+        <Tab.Screen name="Home" component={AlbumScreen} />
+        <Tab.Screen name="My Book" component={AlbumScreen} />
+        <Tab.Screen name="Favorites" component={AlbumScreen} />
+      </Tab.Navigator> 
+    </NavigationContainer> */
   );
 }
 
@@ -120,18 +198,18 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: "#fff"
   },
-  layout:{
-    backgroundColor:"rgba(184,184,184,0.5)"
+  layout: {
+    backgroundColor: "rgba(184,184,184,0.5)"
   },
   headerStyle: {
     width: 304,
     height: 198,
     backgroundColor: "#00b49f",
-    shadowColor:"#000",
-    shadowOffset:{width: 2, height: 0},
-    shadowOpacity:0.1,
-    marginTop:-30,
-    marginBottom:8
+    shadowColor: "#000",
+    shadowOffset: { width: 2, height: 0 },
+    shadowOpacity: 0.1,
+    marginTop: -30,
+    marginBottom: 8
   },
   userimg: {
     width: 70,
@@ -171,12 +249,12 @@ const styles = StyleSheet.create({
     width: 304,
     height: 640,
     backgroundColor: "#ebebeb",
-    shadowColor:"#000",
-    shadowOffset:{width: 56, height: 0},
-    shadowOpacity:0.1
+    shadowColor: "#000",
+    shadowOffset: { width: 56, height: 0 },
+    shadowOpacity: 0.1
   },
-  menu:{
-    marginTop:8
+  menu: {
+    marginTop: 8
   },
   menubox: {
     flexDirection: "row",
@@ -188,23 +266,23 @@ const styles = StyleSheet.create({
   menuimg: {
     width: 24,
     height: 24,
-    marginLeft:24
+    marginLeft: 24
   },
   menuimg2: {
     width: 24,
     height: 24,
-    marginLeft:7
+    marginLeft: 7
   },
-  menutext:{
-    marginLeft:32,
-    width:176,
-    height:18,
-    fontSize:14,
-    fontWeight:"600",
-    color:"#5c5c5c"
+  menutext: {
+    marginLeft: 32,
+    width: 176,
+    height: 18,
+    fontSize: 14,
+    fontWeight: "600",
+    color: "#5c5c5c"
   },
-  layout:{
-    marginTop:8
+  layout: {
+    marginTop: 8
   }
 });
 
